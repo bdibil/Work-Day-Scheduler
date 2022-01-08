bfa3e26109fe1bd2940d18f8f7ae8a335f1106b3
F1_Fan_GoMax! <95992081+bdibil@users.noreply.github.com>
2022-01-07 22:16:42 -0800

    added hour colorizer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,69 @@
 var dayEl = document.querySelector('#currentDay');
-var dummy = 99;
+var textEl = document.querySelectorAll("textarea");
+var currentHour = 10;
+// var timeEl = document.querySelectorAll("time-block");
+
+
+
+var curentHour = 0;
+
+console.log(textEl)
+console.log(curentHour)
+
+function colorReset () {
+    for (let index = 0; index < textEl.length; index++) {
+        // textEl[index].setAttribute("data-state", "future")
+        textEl[index].setAttribute("class", "col-8 future")
+        // console.log(index)
+    }
+    // console.log(textEl)
+}
+
+function getHour(){
+    curentHour = moment().hour(); 
+    colorize()
+}
+
+// var dummy
+
+function colorize(){
+    colorReset()
+    if (curentHour===9) {
+        textEl[0].setAttribute("class", "col-8 present")
+    }
+    if (curentHour>9&&curentHour<18) {
+        var offset = curentHour-9
+        textEl[offset].setAttribute("class", "col-8 present")
+        for (let index = 0; index < offset; index++) {
+            textEl[index].setAttribute("class", "col-8 past")
+        }
+    }
+}
+
+
+
 
 function init() {
     var today = moment().format("dddd, MMMM Do YYYY");
     dayEl.textContent=today
-
-
+    setInterval(getHour,1000)
+    colorReset()
 }
 
 init();
 
- $( function() {
-    $( "#draggable" ).draggable({ scroll: true });
- });
+
+
+
+//  $( function() {
+//     $( "#draggable" ).draggable({ scroll: true });
+//  });
+
+
+
 
 
 //////
-console.log(dummy)
 
 
 ////  OLD CODE      BELOW
